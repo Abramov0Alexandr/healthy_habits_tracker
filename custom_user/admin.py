b7cef54f9d74f6124bc3304953161a20ffad2d29
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-09-01 16:59:43 +0300

    Переопределил модель User, зарегистрировал приложение

--- custom_user/admin.py
+++ custom_user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from custom_user.models import CustomUser
+
+
 # Register your models here.
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'tg_username', 'tg_chat_id', )
+    list_display_links = ('email', )
--- custom_user/models.py
+++ custom_user/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from custom_user.user_manager import CustomUserManager
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class CustomUser(AbstractUser):
+
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='Email')
+    phone = models.CharField(max_length=20, verbose_name='Телефон', **NULLABLE)
+    tg_username = models.CharField(max_length=50, unique=True, verbose_name='TG username')
+    tg_chat_id = models.CharField(max_length=50, unique=True, verbose_name='TG chat-ID', **NULLABLE)
+    is_active = models.BooleanField(default=True, verbose_name='Статус активации')
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
