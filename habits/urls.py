becc3a5c92663bb4c371e83d79a7901c779482ae
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-09-03 14:54:25 +0300

    H_H_TRACKER-3 Написание эндпоинтов модели Habit

---
+++ habits/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .apps import HabitsConfig
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import HabitsListView, HabitsCreateView, PublicHabitsListView, HabitsUpdateView, HabitsDeleteView
+
+
+app_name = HabitsConfig.name
+
+
+urlpatterns = [
+    path('', HabitsListView.as_view(), name='habits_list'),
+    path('public-habits/', PublicHabitsListView.as_view(), name='public_habits_list'),
+    path('create/', HabitsCreateView.as_view(), name='habits_create'),
+    path('create/', HabitsUpdateView.as_view(), name='habits_update'),
+    path('create/', HabitsDeleteView.as_view(), name='habits_delete'),
+
+    # authorization urls
+    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
